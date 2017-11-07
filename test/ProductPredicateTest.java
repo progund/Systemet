a3e9520a235bc9d4158a3cf4013f04d8f30f5ba3
Henrik Sandklef <hesa@sandklef.com>
2017-11-07 20:59:44 +0100

    Test for Product and Predicate

--- test/ProductPredicateTest.java
+++ test/ProductPredicateTest.java
@@ -1,6 +1,7 @@
 import android.util.Log;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 import se.juneday.systemet.domain.Product;
@@ -10,6 +11,7 @@ import se.juneday.systemet.storage.ProductUtil;
 import se.juneday.systemet.storage.FakedProductStore;
 
 public class ProductPredicateTest{
+
   private static final String LOG_TAG = ProductPredicateTest.class.getName();
 
   public static void main(String[] args) {
@@ -22,22 +24,30 @@ public class ProductPredicateTest{
       FakedProductStore.getInstance(null);
 
     productPredicates.add(new ProductPredicate(ProductPredicate.VARIABLES.PRICE,
-                                        ProductPredicate.OPERATIONS.GT,
-                                        "12"));
+                                               ProductPredicate.OPERATIONS.GT,
+                                               "5"));
+    
+    productPredicates.add(new ProductPredicate(ProductPredicate.VARIABLES.PRICE,
+                                               ProductPredicate.OPERATIONS.LT,
+                                               "200"));
+    
+    productPredicates.add(new ProductPredicate(ProductPredicate.VARIABLES.TYPE,
+                                               ProductPredicate.OPERATIONS.EQ,
+                                               "Öl"));
     
     //    predicates.add((p) -> true);
     Predicate<Product> predicates = (p) -> true;
     
     for (ProductPredicate predicate : productPredicates) {
-      predicate.setValue("13");
       predicates = predicates.and(predicate);
-      System.out.println(" * " + predicate);
+      //      System.out.println(" * " + predicate);
     }
 
     products = store.products();
     Log.d(LOG_TAG, " * products: " + products.size());
     products = ProductUtil.getProductsFilteredBy(products, predicates);
     Log.d(LOG_TAG, " * products: " + products.size());
+    Log.d(LOG_TAG, " * products: " + products);
 
   }
 
